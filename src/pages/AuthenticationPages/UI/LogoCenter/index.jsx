c31b3b58ac8d5e39d6d47760f6f72a8cd443e4eb
LeoVo <Leo.Volkov156@gmail.com>
2025-02-11 14:15:46 +0300

    zero

--- src/pages/AuthenticationPages/UI/LogoCenter/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { logo } from '@images'
-
-const LogoCenter = () => {
-  return <img src={logo} alt="Логотип" className="mx-auto" />
-}
-
-export default LogoCenter
