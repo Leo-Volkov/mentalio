4a02e62202f3a10badecf1749f2de999b94f26e3
LeoVo <Leo.Volkov156@gmail.com>
2025-02-11 13:54:22 +0300

    0v FSD

---
+++ src/entities/userTests/index.ts
@@ -0,0 +1,3 @@
+import type { Test, Question, Answer } from './model/types'
+
+export type { Test, Question, Answer }
---
+++ src/pages/TestPage/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export function TestPage() {
+  return <h1>TestPage</h1>
+}
